777c58e33b07352bf42a011d7401e85fa85d49f2
Pascal.Kipkemoi <Pascal.Kipkemoi@KE-HQ-000925.M-KOPA.local>
2018-01-15 16:55:27 +0300

    my first commit

---
+++ Object Repository/Payments/Confirmation Sms On Payment/payment menu.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>payment menu</name>
+   <tag></tag>
+   <elementGuidId>e7b673df-0267-4469-afb3-bb8d1bc6f0cd</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>.//*[@id='Menu']/li[4]/span</value>
+   </webElementProperties>
+</WebElementEntity>
