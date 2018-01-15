c8f326013e649e7807486739fd333e794c162620
dennis.gituto <dennis.gituto@KE-HQ-001711.M-KOPA.local>
2018-01-15 12:30:00 +0300

    Object Repo Folders

---
+++ Object Repository/Payments/Upload Payments(MPesa)/Upload Button.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Upload Button</name>
+   <tag></tag>
+   <elementGuidId>c5d486fd-6b50-4924-b88c-b0a97f2fdb1a</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
