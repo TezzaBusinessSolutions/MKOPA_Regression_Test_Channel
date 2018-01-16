c65f5c3ba32a1a531e231c8437c1c3d3ba43d5d7
dennis.gituto <dennis.gituto@KE-HQ-001711.M-KOPA.local>
2018-01-16 16:49:24 +0300

    Updated Upload File Center Common Scripts

---
+++ Object Repository/File Upload Center/File Upload Center Page/btnClosePopUpWindow.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>The button to close the window dialog propmpting success Message</description>
+   <name>btnClosePopUpWindow</name>
+   <tag></tag>
+   <elementGuidId>b6719cc9-863d-459e-a569-1d07fcf9835d</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>.//*[@id='file-upload-center-paymentfile-window']//span[text()=&quot;Close&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>
