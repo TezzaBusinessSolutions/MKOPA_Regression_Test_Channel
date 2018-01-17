64f0053b4a18826d16a87d472ba564787fb79ae9
dennis.gituto <dennis.gituto@KE-HQ-001711.M-KOPA.local>
2018-01-17 19:34:07 +0300

    # WARNING: head commit changed in the meantime
    Dealer Payments Scripts and some few Changes

---
+++ Object Repository/Dealers/Common Dealers Objects/mnu-Dealers DropDown.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Dealers Link in the Dashboard</description>
+   <name>mnu-Dealers DropDown</name>
+   <tag></tag>
+   <elementGuidId>5b416031-22de-4b45-a95e-644ac630867d</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>.//*[@id='Menu']//span[text()=&quot;Dealers&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>
