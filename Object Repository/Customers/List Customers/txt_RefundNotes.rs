f31a949b97a8a550a25a22cad24ca306b1310121
charles.chweyaa <charles.chweyaa@KE-HQ-001644.M-KOPA.local>
2018-01-18 09:13:10 +0300

    18/01/2918 Commit

---
+++ Object Repository/Customers/List Customers/txt_RefundNotes.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>txt_RefundNotes</name>
+   <tag></tag>
+   <elementGuidId>82f3205b-b369-4ece-9ba6-b3949261541f</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
