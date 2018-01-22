1266d34c8cebbdd49dc298a384e3f23d49a2ce90
charles.chweyaa <charles.chweyaa@KE-HQ-001644.M-KOPA.local>
2018-01-22 15:56:10 +0300

    commit afte

---
+++ Test Suites/Products/PaymentsPlanApproval.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>PaymentsPlanApproval</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-01-18T15:43:58</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>00065d3e-f108-47ee-9967-31fcf12efba6</testSuiteGuid>
+</TestSuiteEntity>
